ce8d20307ba42b75651f4cbf369c3178ab784a63
vinh <doanngocvinh1@gmail.com>
2023-12-11 03:39:56 +0700

    .

--- scripts/extra.sh
+++ scripts/extra.sh
@@ -4,6 +4,15 @@ docker cp ./distinct hadoop-master://root
 docker cp ./mpd hadoop-master://root
 docker cp ./mean hadoop-master://root
 
+docker cp hadoop-master://root/wordcount/output output/wordcount
+docker cp hadoop-master://root/gnome/output output/gnome
+docker cp hadoop-master://root/distinct/output output/distinct
+docker cp hadoop-master://root/mean/output output/mean
+docker cp hadoop-master://root/mpd/output output/mpd
+
+
+
+
 
 hdfs dfs -mkdir /wordcount
 hdfs dfs -mkdir /wordcount/input
